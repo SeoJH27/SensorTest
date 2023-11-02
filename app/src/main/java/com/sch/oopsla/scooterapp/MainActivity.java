e0b39999134ad2002c09e3c2a7173ccc761bd487
SeoJH27 <morninglibrary@gmail.com>
2023-11-02 18:27:35 +0900

    정상 범위를 정해야 함!

--- app/src/main/java/com/sch/oopsla/scooterapp/MainActivity.java
+++ app/src/main/java/com/sch/oopsla/scooterapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.sch.oopsla.scooterapp;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.hardware.Sensor;
@@ -9,109 +10,194 @@ import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.ColorInt;
 import androidx.appcompat.app.AppCompatActivity;
 
-
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
-    private static final int SHAKE_SKIP_TIME = 500;
-    private static final float SHAKE_THRESHORLD_GRAVITY = 2.7f;
-    int num = 0; int abs = 0; boolean flag = false;
-    float[] gravity, magnetic;
-    float accels[] = new float[3];
-    float mags[] = new float[3];
-    float[] values = new float[3];
-    float azimuth, pitch, roll;
-    TextView text1, text, sensing;
-    Sensor Accelerometer, Magnetometer;
-    SensorManager sManager;
+    TextView text, sensing, list;
+    Button button, CKbutton, REbutton;
+    boolean buttonF = false;
+    int i = 1;
 
-    public void onAccuracyChanged(Sensor sensor, int accuracy){
+    private SensorManager mSensorManager = null;
+    private Sensor mGyroscopeSensor = null;
+    private Sensor mAccelerometer = null;
+    UserSensorListner userSensorListner;
 
-    }
+    /*Sensor variables*/
+    private float[] mGyroValues = new float[3];
+    private float[] mAccValues = new float[3];
+    private double mAccPitch, mAccRoll;
 
-    public void onSensorChanged(SensorEvent event){
-        switch (event.sensor.getType()){
-            case Sensor.TYPE_MAGNETIC_FIELD:
-                mags = event.values.clone();
-                break;
-            case Sensor.TYPE_ACCELEROMETER:
-                accels = event.values.clone();
-                break;
-        }
+    private float a = 0.2f;
+    private static final float NS2S = 1.0f/1000000000.0f;
+    private double pitch = 0, roll = 0;
+    private double timestamp;
+    private double dt;
+    private double temp;
 
-        if(mags != null && accels != null){
-            gravity = new float[9];
-            magnetic = new float[9];
-            SensorManager.getRotationMatrix(gravity, magnetic, accels, mags);
-            
-            //보정된 방향 데이터
-            float[] outGravity = new float[9];
-            SensorManager.remapCoordinateSystem(gravity, SensorManager.AXIS_X, SensorManager.AXIS_Z, outGravity);
-            SensorManager.getOrientation(outGravity, values);
+    private boolean gyroRunning;
+    private boolean accRunning;
 
 
-            azimuth = values[2] * 180.0f / 3.14f;
-            pitch = values[1] * 180.0f / 3.14f;
-            roll = values[0] * 180.0f / 3.14f;
-            mags = null;
-            accels = null;
+    public void onAccuracyChanged(Sensor sensor, int accuracy){}
+    public void onSensorChanged(SensorEvent event){}
 
-            if(num == 1){
-                flag = true;
-                text.setText("pitch= " + pitch + "\nroll= " + roll + "\n");
-                abs = Math.abs((int)pitch) + Math.abs((int)roll);
-                Log.e("y", ""+abs);
-                num += 2;
-            }
-            else if (num == 0) num += 1;
-
-            text1.setText("pitch= " + pitch + "\nroll= " + roll + "\n");
-
-            if(flag) {
-                if (Math.abs(abs - Math.abs((int) pitch) - Math.abs((int) roll)) >= 50){
-                    sensing.setTextSize(20);
-                    sensing.setTextColor(Color.parseColor("#FF0000"));
-                    sensing.setText("비정상");
-                }
-                else{
-                    sensing.setTextColor(Color.parseColor("#000000"));
-                    sensing.setTextSize(14);
-                    sensing.setText("정상");
-
-                }
-            }
-        }
-    }
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        text1 = (TextView) findViewById(R.id.testView2);
-        text = (TextView) findViewById(R.id.testView1);
+        text = (TextView) findViewById(R.id.testView2);
         sensing = (TextView) findViewById(R.id.sensing);
-        sManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-        Accelerometer = sManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
-        Magnetometer = sManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        button = (Button) findViewById(R.id.button);
+        list = (TextView) findViewById(R.id.list);
+        CKbutton = (Button) findViewById(R.id.CKbutton);
+        REbutton = (Button) findViewById(R.id.REbutton);
+        userSensorListner = new UserSensorListner();
+
+        mSensorManager = (SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        mGyroscopeSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
+        mAccelerometer= mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (buttonF == false){
+                    button.setBackgroundColor(Color.parseColor("#FF0000"));
+                    button.setText("시작");
+                    button.setTextColor(getResources().getColor(R.color.black));
+                        if (mAccelerometer != null){
+                            mSensorManager.registerListener(userSensorListner, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+                        }
+                        if(mGyroscopeSensor != null){
+                            mSensorManager.registerListener(userSensorListner, mGyroscopeSensor, SensorManager.SENSOR_DELAY_NORMAL);
+                        }
+                    buttonF = true;
+
+
+                    if ((roll > -40.0 && roll <90)){
+                        sensing.setTextColor(Color.parseColor("#000000"));
+                        sensing.setTextSize(14);
+                        sensing.setText("정상");
+                    }
+                    else{
+                        sensing.setTextSize(20);
+                        sensing.setTextColor(Color.parseColor("#FF0000"));
+                        sensing.setText("비정상");
+                    }
+
+                }
+                else{
+                    mSensorManager.unregisterListener(userSensorListner);
+                    button.setBackgroundColor(Color.parseColor("#000000"));
+                    button.setText("보정");
+                    button.setTextColor(getResources().getColor(R.color.white));
+                    buttonF = false;
+                }
+            }
+        });
+
+        CKbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.append(i + " " + "roll : "+String.format("%.2f", roll)+"\t\t\tpitch : "+String.format("%.2f", pitch) + "\n");
+                i+=1;
+            }
+        });
+
+        REbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                list.setText("");
+                i = 1;
+            }
+        });
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        if (Accelerometer != null){
-            sManager.registerListener(this, Accelerometer, SensorManager.SENSOR_DELAY_NORMAL);
-        }
-        if(Magnetometer != null){
-            sManager.registerListener(this, Magnetometer, SensorManager.SENSOR_DELAY_NORMAL);
-        }
     }
-
     @Override
     protected void onStop(){
         super.onStop();
-        sManager.unregisterListener(this);
+    }
+
+
+    private void complementaty(double new_ts){
+
+        /* 자이로랑 가속 해제 */
+        gyroRunning = false;
+        accRunning = false;
+
+        /*센서 값 첫 출력시 dt(=timestamp - event.timestamp)에 오차가 생기므로 처음엔 break */
+        if(timestamp == 0){
+            timestamp = new_ts;
+            return;
+        }
+        dt = (new_ts - timestamp) * NS2S; // ns->s 변환
+        timestamp = new_ts;
+
+        /* degree measure for accelerometer */
+        mAccPitch = -Math.atan2(mAccValues[0], mAccValues[2]) * 180.0 / Math.PI; // Y 축 기준
+        mAccRoll= Math.atan2(mAccValues[1], mAccValues[2]) * 180.0 / Math.PI; // X 축 기준
+
+        /**
+         * 1st complementary filter.
+         *  mGyroValuess : 각속도 성분.
+         *  mAccPitch : 가속도계를 통해 얻어낸 회전각.
+         */
+        temp = (1/a) * (mAccPitch - pitch) + mGyroValues[1];
+        pitch = pitch + (temp*dt);
+
+        temp = (1/a) * (mAccRoll - roll) + mGyroValues[0];
+        roll = roll + (temp*dt);
+
+        text.setText("roll : "+String.format("%.2f", roll)+"\t\t\tpitch : "+String.format("%.2f", pitch));
+    }
+
+    public class UserSensorListner implements SensorEventListener{
+
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            switch (event.sensor.getType()){
+
+                /** GYROSCOPE */
+                case Sensor.TYPE_GYROSCOPE:
+
+                    /*센서 값을 mGyroValues에 저장*/
+                    mGyroValues = event.values;
+
+                    if(!gyroRunning)
+                        gyroRunning = true;
+
+                    break;
+
+                /** ACCELEROMETER */
+                case Sensor.TYPE_ACCELEROMETER:
+
+                    /*센서 값을 mAccValues에 저장*/
+                    mAccValues = event.values;
+
+                    if(!accRunning)
+                        accRunning = true;
+
+                    break;
+
+            }
+
+            /**두 센서 새로운 값을 받으면 상보필터 적용*/
+            if(gyroRunning && accRunning){
+                complementaty(event.timestamp);
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {}
     }
 }
